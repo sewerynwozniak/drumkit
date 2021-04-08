1040991c56ae1d9bed650bde167a7f592ed2c37d
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-04-08 08:28:56 +0200

    creating reading key and playing sound function

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-document.addEventListener('keydown', readKey);
+var hihat = document.querySelector('[data-sound="hihat"]');
+var perc = document.querySelector('[data-sound="perc"]');
+var btn = document.querySelector('.btn--play');
+var channelPlay = [];
+appStart();
+function appStart() {
+    document.addEventListener('keydown', readKey);
+    btn.addEventListener('click', playChannel);
+}
+function playChannel() {
+    console.log('działa');
+}
 function readKey(e) {
-    console.log(e.key);
+    var key = e.key;
+    var time = e.timeStamp;
+    // tylko wybrane klawisze mają być pushowane i odtwarzane
+    channelPlay.push(key, time);
+    console.log(channelPlay);
+    switch (key) {
+        case 'a':
+            hihat.play();
+            hihat.currentTime = 0;
+            break;
+        case 's':
+            perc.play();
+            perc.currentTime = 0;
+            break;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,37 @@
-document.addEventListener('keydown', readKey);
+const hihat : HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
+const perc : HTMLAudioElement = document.querySelector('[data-sound="perc"]');
+const btn : HTMLButtonElement = document.querySelector('.btn--play');
+const channelPlay:any[] =[];
+
+appStart();
+function appStart():void{
+    document.addEventListener('keydown', readKey);
+    btn.addEventListener('click', playChannel);
+}
 
 
-function readKey(e){
-    console.log(e.key)
+function playChannel():void{
+    console.log('działa')
+}
+
+
+function readKey(e :KeyboardEvent) :void{
+    const key = e.key;
+    const time = e.timeStamp;
+    // tylko wybrane klawisze mają być pushowane i odtwarzane
+    channelPlay.push(key, time);
+    console.log(channelPlay)
+
+    switch(key){
+        
+        case 'a':
+            hihat.play();
+            hihat.currentTime=0;
+            break;
+        case 's':
+            perc.play();
+            perc.currentTime=0;
+            break;
+    }
+  
 }
