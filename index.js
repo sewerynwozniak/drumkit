8066e9ef6f6f04b3b0591cba3f4bb598c39ae2dc
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-04-09 07:57:50 +0200

    styling interface

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
 var hihat = document.querySelector('[data-sound="hihat"]');
-var perc = document.querySelector('[data-sound="perc"]');
-var btn = document.querySelector('.btn--play');
+var kick = document.querySelector('[data-sound="kick"]');
+var snare = document.querySelector('[data-sound="snare"]');
+var ride = document.querySelector('[data-sound="ride"]');
+var hihatDiv = document.querySelector('#hihatDiv');
+var kickDiv = document.querySelector('#kickDiv');
+var snareDiv = document.querySelector('#snareDiv');
+var rideDiv = document.querySelector('#rideDiv');
+var btnRecord = document.querySelector('.btn--record');
+var btnPlay = document.querySelector('.btn--play');
 var channelPlay = [];
+var recordTimeStamp = [];
 appStart();
 function appStart() {
     document.addEventListener('keydown', readKey);
-    btn.addEventListener('click', playChannel);
+    btnRecord.addEventListener('click', record);
+    btnPlay.addEventListener('click', playChannel);
+}
+function record(e) {
+    var singleRecordTimeStamp = e.timeStamp;
+    recordTimeStamp.push({ 'ch1': singleRecordTimeStamp });
 }
 function playChannel() {
-    console.log('działa');
+    console.log(recordTimeStamp[0].ch1);
+    console.log(channelPlay);
+    channelPlay.forEach(function (sound) {
+        var time = recordTimeStamp[0].ch1 - sound.time;
+        setTimeout(function () { return playSound(sound.key); }, time);
+    });
 }
 function readKey(e) {
     var key = e.key;
     var time = e.timeStamp;
     // tylko wybrane klawisze mają być pushowane i odtwarzane
-    channelPlay.push(key, time);
-    console.log(channelPlay);
+    channelPlay.push({ key: key, time: time });
+    playSound(key);
+    animationBlock(key);
+}
+function addAnimation(el) {
+    el.style.transform = 'scale(0.95)';
+    setTimeout(function () {
+        el.style.transform = 'scale(1)';
+    }, 10);
+}
+function animationBlock(key) {
     switch (key) {
-        case 'a':
+        case 'q':
+            addAnimation(hihatDiv);
+            break;
+        case 'w':
+            addAnimation(kickDiv);
+            break;
+        case 'e':
+            addAnimation(snareDiv);
+            break;
+        case 'r':
+            addAnimation(rideDiv);
+            break;
+    }
+}
+function playSound(key) {
+    switch (key) {
+        case 'q':
             hihat.play();
             hihat.currentTime = 0;
             break;
-        case 's':
-            perc.play();
-            perc.currentTime = 0;
+        case 'w':
+            kick.play();
+            kick.currentTime = 0;
+            break;
+        case 'e':
+            snare.play();
+            snare.currentTime = 0;
+            break;
+        case 'r':
+            ride.play();
+            ride.currentTime = 0;
             break;
     }
 }
--- index.ts
+++ index.ts
@@ -1,17 +1,39 @@
 const hihat : HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
-const perc : HTMLAudioElement = document.querySelector('[data-sound="perc"]');
-const btn : HTMLButtonElement = document.querySelector('.btn--play');
+const kick : HTMLAudioElement = document.querySelector('[data-sound="kick"]');
+const snare : HTMLAudioElement = document.querySelector('[data-sound="snare"]');
+const ride : HTMLAudioElement = document.querySelector('[data-sound="ride"]');
+
+const hihatDiv : HTMLDivElement = document.querySelector('#hihatDiv');
+const kickDiv : HTMLDivElement = document.querySelector('#kickDiv');
+const snareDiv : HTMLDivElement = document.querySelector('#snareDiv');
+const rideDiv : HTMLDivElement = document.querySelector('#rideDiv');
+
+const btnRecord : HTMLButtonElement = document.querySelector('.btn--record');
+const btnPlay : HTMLButtonElement = document.querySelector('.btn--play');
 const channelPlay:any[] =[];
+const recordTimeStamp:any[]=[];
 
 appStart();
 function appStart():void{
     document.addEventListener('keydown', readKey);
-    btn.addEventListener('click', playChannel);
+    btnRecord.addEventListener('click', record);
+    btnPlay.addEventListener('click', playChannel);
+}
+
+
+function record(e :KeyboardEvent):void{
+const singleRecordTimeStamp = e.timeStamp;
+recordTimeStamp.push({'ch1': singleRecordTimeStamp});
 }
 
 
 function playChannel():void{
-    console.log('działa')
+    console.log(recordTimeStamp[0].ch1)
+    console.log(channelPlay)
+    channelPlay.forEach((sound)=>{
+        let time = recordTimeStamp[0].ch1 - sound.time;
+        setTimeout(()=>playSound(sound.key), time) 
+    })
 }
 
 
@@ -19,19 +41,57 @@ function readKey(e :KeyboardEvent) :void{
     const key = e.key;
     const time = e.timeStamp;
     // tylko wybrane klawisze mają być pushowane i odtwarzane
-    channelPlay.push(key, time);
-    console.log(channelPlay)
+    channelPlay.push({key, time});
+   
+    playSound(key);
+    animationBlock(key)
+}
 
-    switch(key){
-        
-        case 'a':
-            hihat.play();
-            hihat.currentTime=0;
+
+function addAnimation(el:HTMLDivElement):void{
+el.style.transform = 'scale(0.95)';
+setTimeout(()=>{
+    el.style.transform = 'scale(1)';
+},10)
+}
+
+
+function animationBlock(key: string):void{
+
+    switch (key) {
+        case 'q':
+            addAnimation(hihatDiv);
             break;
-        case 's':
-            perc.play();
-            perc.currentTime=0;
+        case 'w':
+            addAnimation(kickDiv);
+            break;
+        case 'e':
+            addAnimation(snareDiv);
+            break;
+        case 'r':
+            addAnimation(rideDiv);
             break;
     }
-  
+
+}
+
+function playSound(key: string):void {
+    switch (key) {
+        case 'q':
+            hihat.play();
+            hihat.currentTime = 0;
+            break;
+        case 'w':
+            kick.play();
+            kick.currentTime = 0;
+            break;
+        case 'e':
+            snare.play();
+            snare.currentTime = 0;
+            break;
+        case 'r':
+            ride.play();
+            ride.currentTime = 0;
+            break;
+    }
 }
